135e33973e3c2c36db98637d85962673d6a6b720
manbing.li <manbing.li@cern.ch>
2024-06-06 17:01:38 +0200

    adding detector volume

--- construction.cc
+++ construction.cc
@@ -8,8 +8,37 @@ MyDetectorConstruction::~MyDetectorConstruction()
 G4VPhysicalVolume *MyDetectorConstruction::Construct()
 {
   G4NistManager *nist = G4NistManager::Instance();
-  G4Material *worldMat = nist->FindOrBuildMaterial("G4_AIR");
 
+  //Agl cherenkov detector
+  G4Material *SiO2 = new G4Material("SiO2", 2.201*g/cm3, 2);
+  SiO2->AddElement(nist->FindOrBuildElement("Si"), 1);
+  SiO2->AddElement(nist->FindOrBuildElement("O"), 2);
+
+  G4Material *H2O = new G4Material("H2O", 1.000*g/cm3, 2);
+  H2O->AddElement(nist->FindOrBuildElement("H"), 2);
+  H2O->AddElement(nist->FindOrBuildElement("O"), 1);
+
+  G4Element *C = nist->FindOrBuildElement("C");
+
+  G4Material *Aerogel = new G4Material("Aerogel", 0.200*g/cm3, 3);
+  Aerogel->AddMaterial(SiO2, 62.5*perCent);
+  Aerogel->AddMaterial(H2O, 37.4*perCent);
+  Aerogel->AddElement(C, 0.1*perCent);
+
+  //add reflective index to the properties
+  G4double energy[2] = {1.239841939*eV/0.9, 1.239841939*eV/0.2};
+  G4double rindexAerogel[2] = {1.1, 1.1};
+  G4double rindexWorld[2] = {1.0, 1.0};
+  G4MaterialPropertiesTable *mptAerogel = new G4MaterialPropertiesTable();
+  mptAerogel->AddProperty("RINDEX", energy, rindexAerogel, 2);
+  Aerogel->SetMaterialPropertiesTable(mptAerogel);
+  
+  G4Material *worldMat = nist->FindOrBuildMaterial("G4_AIR");
+  //another G4MaterialPropertiesTable
+  G4MaterialPropertiesTable *mptWorld = new G4MaterialPropertiesTable();
+  mptWorld->AddProperty("RINDEX", energy, rindexWorld, 2);
+  
+  
   //create a world, volume 
   //half of the legthen, default is mm, multiply with m, with G4SystemOfUnits
   G4Box *solidWorld = new G4Box("solidWorld", 0.5*m, 0.5*m, 0.5*m);
@@ -17,8 +46,17 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
   //insert material to this volume
   G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
 
+  
+
   //create physical volume, rotation(0), center of the Geant4 coordiate, look up all variables meaning
   G4VPhysicalVolume *physWorld = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), logicWorld, "pyhsWorld", 0, false, 0, true);
+
+  G4Box *solidRadiator = new G4Box("solidRadiator", 0.4*m, 0.4*m, 0.01*m);
+  G4LogicalVolume *logicRadiator = new G4LogicalVolume(solidRadiator, Aerogel,  "logicalRadiator");
+
+  //define physical volume (daughter volume of the physWorld)
+  G4VPhysicalVolume *physRadiator = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.25*m), logicRadiator, "physRadiator", logicWorld, false, 0, true);
+  
 
   return physWorld;
   
